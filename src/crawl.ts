db398186d23fd54018c6de87b4215897d37e0e43
hrishix6 <hrishi.pethe@gmail.com>
2024-02-27 00:27:36 +0530

    refactor: move from iterative to recursive crawling

--- src/crawl.ts
+++ src/crawl.ts
@@ -5,11 +5,15 @@ export function normalizeURL(url: string) {
     const parsedUrl = new URL(url);
     return `${parsedUrl.hostname}${parsedUrl.pathname}`;
 }
-
-interface URLInfo {
+export interface URLInfo {
     url: string;
     internalLinks: string[];
     externalLinks: string[];
+}
+export interface LinksCount {
+    internalLinksCount: number;
+    extLinksCount: number;
+    visited: number;
 }
 
 export function getUrlsFromHtml(url: string, html: string, baseUrl: string) {
@@ -32,7 +36,7 @@ export function getUrlsFromHtml(url: string, html: string, baseUrl: string) {
 
     for (let link of linkEls) {
 
-        if (IsInternalUrl(link.href, baseUrl)) {
+        if (isInternalUrl(link.href, baseUrl)) {
             info.internalLinks.push(link.href);
         }
         else {
@@ -44,10 +48,9 @@ export function getUrlsFromHtml(url: string, html: string, baseUrl: string) {
     return info;
 }
 
-export function IsInternalUrl(url: string, baseUrl: string) {
+export function isInternalUrl(url: string, baseUrl: string) {
     return new URL(url).hostname === new URL(baseUrl).hostname;
 }
-
 
 export async function fetchPage(url: string): Promise<string | null> {
 
@@ -71,3 +74,47 @@ export async function fetchPage(url: string): Promise<string | null> {
 }
 
 export const pause = (ms: number) => new Promise(res => setTimeout(res, ms));
+
+export async function crawl(baseUrl: string, urls: string[], info: Record<string, LinksCount>, delayMs: number) {
+
+    if(urls.length == 0) {
+        return info;
+    }
+
+    const current = urls.pop();
+
+    if(!current) {
+        return await crawl(baseUrl, urls,info,delayMs);
+    }
+
+    const normalizedUrl = normalizeURL(current);
+
+    if(normalizedUrl in info){
+        info[normalizedUrl].visited++;
+        return await crawl(baseUrl, urls,info,delayMs);
+    }
+
+    const html = await fetchPage(current);
+    if (!html) {
+        console.log(`No html content found at ${current}, moving on.`);
+        info[normalizedUrl] = {
+            extLinksCount: 0,
+            internalLinksCount: 0,
+            visited: 1
+        };
+        return await crawl(baseUrl, urls,info,delayMs);
+    }
+    
+    console.log(`Crawled ${current}, extracting urls..`);
+
+    const meta = getUrlsFromHtml(current, html, baseUrl);
+    const { url, externalLinks, internalLinks } = meta;
+    info[url] = { visited: 1, extLinksCount: externalLinks.length, internalLinksCount: internalLinks.length };
+
+    urls.push(...internalLinks);
+
+    //artificial delay to not cause spam.
+    console.log('drinking coffee...');
+    await pause(delayMs);
+    return await crawl(baseUrl,urls,info,delayMs);
+}
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
-import { normalizeURL, fetchPage, getUrlsFromHtml, pause } from "./crawl";
+import { url } from "inspector";
+import { normalizeURL, fetchPage, getUrlsFromHtml, pause, LinksCount, crawl } from "./crawl";
 
 async function main() {
     try {
@@ -18,61 +19,9 @@ async function main() {
         const urls: string[] = [];
         const info: Record<string, { internalLinksCount: number, extLinksCount: number, visited: number }> = {};
         const start = new Date();
-        console.log(`crawling base url...`);
-
-        const html = await fetchPage(baseUrl);
-
-        if (!html) {
-            console.log(`No html content found at baseurl ${baseUrl}, exiting...`);
-            process.exit(0);
-        }
-
-        const { externalLinks, internalLinks, url } = getUrlsFromHtml(baseUrl, html, baseUrl);
-        info[url] = {
-            visited: 1,
-            extLinksCount: externalLinks.length,
-            internalLinksCount: internalLinks.length
-        };
-
-        urls.push(...internalLinks);
-
-        while (urls.length) {
-            const current = urls.pop();
-            if (!current) {
-                continue;
-            }
-
-            const normalizedUrl = normalizeURL(current);
-
-            if (normalizedUrl in info) {
-                info[normalizedUrl].visited++;
-                continue;
-            }
-
-            const html = await fetchPage(current);
-            if (!html) {
-                console.log(`No html content found at ${current}, moving on.`);
-                info[normalizedUrl] = {
-                    extLinksCount: 0,
-                    internalLinksCount: 0,
-                    visited: 1
-                };
-                continue;
-            }
-
-            console.log(`Crawled ${current}, extracting urls..`);
-            const meta = getUrlsFromHtml(current, html, baseUrl);
-
-            const { url, externalLinks, internalLinks } = meta;
-
-            info[url] = { visited: 1, extLinksCount: externalLinks.length, internalLinksCount: internalLinks.length };
-
-            urls.push(...internalLinks);
-
-            //artificial delay to not cause spam.
-            console.log('drinking coffee...');
-            await pause(2500);
-        }
+        urls.push(baseUrl);
+        
+        await crawl(baseUrl,urls,info,1500);
 
         const end = new Date();
         console.log(`Finished crawling ${baseUrl} in ${end.getTime() - start.getTime()}ms`);
